28fa113ae615accfe9e11c1dd216dfbe20af4e85
tuandtph13846 <tuandtph13846@fpt.edu.vn>
2022-10-18 23:36:40 +0700

    layout admin

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import "antd/dist/antd.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
 import { Route, Routes } from "react-router-dom";
 import WebsiteLayout from "./layouts/WebsiteLayout";
 import Homepage from "./pages/client/Homepage";
+import AdminLayout from "./layouts/admin/AdminLayout";
+import Dashboard from "./pages/admin/Dashboard";
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<WebsiteLayout />}>
           <Route index element={<Homepage />} />
+        </Route>
+        <Route path="admin" element={<AdminLayout/>}>
+        <Route path="dashboard" element={<Dashboard />} />
         </Route>
       </Routes>
     </div>
